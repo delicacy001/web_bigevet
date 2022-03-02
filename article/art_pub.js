994c8978ddaff1deae82b369f64b06ed20492315
jinzhao <15138646570@163.com>
2022-03-02 11:51:14 +0800

    文章列表以及文章发表已经搞定，差文章的编辑与删除啦

--- article/art_pub.js
+++ article/art_pub.js
@@ -2,14 +2,6 @@ $(function() {
     //文章分类渲染
     initSelect();
 
-    function render(data) {
-        var str = '<select name="selectCat" lay-verify="required"><option value="">请选择文章类别</option>';
-        for (var i = 0; i < data.length; i++) {
-            str += "<option value='" + i + "'>" + data.name + "</option></select>";
-        }
-        return str;
-    }
-
     function initSelect() {
         $.ajax({
             method: 'GET',
@@ -20,13 +12,23 @@ $(function() {
                     return layer.msg('获取文章分类列表失败！');
                 }
                 console.log(res);
-                layer.msg('获取文章分类列表成功！');
-                $('#selectList').empty().html(render(res.data));
-                // var strHtm = template('tplSelect', res);
-                // // $("#selectList").empty().html(strHtml);
-                // $('#selectList').html(strHtm);
+                renderSelect(res.data);
+                // var strHtml = template('tplSelect', res);
+                // $("#selectList").empty().html(strHtml);
+                // layer.msg('获取文章分类列表成功！');
             }
         });
+    }
+    // 初始化富文本编辑器
+    initEditor();
+    //使用layui的方法可以成功渲染
+    function renderSelect(data) {
+        var html = '<option value="">请选择文章类别</option>'
+        for (var i = 0; i < data.length; i++) {
+            html += '<option value=' + parseInt(data[i].Id) + '>' + data[i].name + '</option>';
+        }
+        $('#selectList').html(html);
+        layui.form.render("select");
     }
     // 封面图片部分js代码
     var layer = layui.layer;
@@ -35,7 +37,7 @@ $(function() {
     // 1.2 配置选项
     const options = {
         // 纵横比
-        aspectRatio: 16 / 12,
+        aspectRatio: 400 / 280,
         // 指定预览区域
         preview: '.img-preview'
     }
@@ -43,7 +45,7 @@ $(function() {
     // 1.3 创建裁剪区域
     $image.cropper(options);
     //2、实现图片文件上传功能
-    //首先，需要为上传按钮绑定点击事件
+    //首先，需要为选择封面按钮绑定点击事件
     $('#btnUpload').on('click', function() {
         $('#file').click();
     });
@@ -61,37 +63,71 @@ $(function() {
         // 重新设置图片路径
         // 重新初始化裁剪区域
         $image.cropper('destroy').attr('src', newImgURL).cropper(options);
-
-        // $('#btnConfirm').on('click', function() {
-        //     // 3. 将裁剪后的图片，输出为 base64 格式的字符串
-        //     // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
-        //     var dataURL = $image.cropper('getCroppedCanvas', {
-        //         // 创建一个 Canvas 画布
-        //         width: 100,
-        //         height: 100
-        //     }).toDataURL('image/png');
-        //     $.ajax({
-        //         method: 'POST',
-        //         // url根目录地址（baseApi中统一设置）
-        //         url: '/my/update/avatar',
-        //         // headers请求头配置对象（baseApi中统一设置）
-        //         // headers: {
-        //         //     Authorization: localStorage.getItem('token')
-        //         // },
-        //         data: { avatar: dataURL },
-        //         success: function(res) {
-        //             if (res.status !== 0) {
-        //                 return layer.msg("更换头像失败！");
-        //             }
-        //             // console.log(res);
-        //             layer.msg('更换头像成功！');
-        //             //调用父页面中的方法，重新渲染用户的头像和用户的信息
-        //             parent.window.getUserInfo();
-
-        //         }
-        //     });
+    });
+    // 定义文章的发布状态
+    var art_state = '已发布';
+    // 为存为草稿按钮，绑定点击事件
+    $('#saveFor').on("click", function(e) {
+        e.preventDefault();
+        art_state = '草稿';
+    });
+    // 为表单绑定submit提交事件
+    $('#pubForm').on("submit", function(e) {
+        // 1、阻止表单默认提交行为
+        e.preventDefault();
+        // 2、基于表单创建FormData对象
+        var fd = new FormData($(this)[0]);
+        // 3、将文章的发布状态存到fd中
+        fd.append('state', art_state);
+        // 4、将图片blob文件存到fd中
+        // 将裁剪后的图片，输出为 文件对象
+        // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        // console.log("打印一下formdata对象");
+        // fd.forEach(function(v, k) {
+        //     console.log(k, v);
         // });
+        // console.log("打印一下formdata对象");
+        $image.cropper('getCroppedCanvas', {
+            // 创建一个 Canvas 画布
+            width: 400,
+            height: 280
+        }).toBlob(function(blob) { // 将 Canvas 画布上的内容，转化为文件对象
+            // 得到文件对象后，进行后续的操作
+            console.log("打印一下blob对象");
+            console.log(blob);
+            console.log("打印一下blob对象");
+            fd.append('cover_img', blob);
+            console.log("打印一下formdata对象");
+            fd.forEach(function(v, k) {
+                console.log(k, v);
+            });
+            console.log("打印一下formdata对象");
+            // 发送ajax请求
+            publishArticle(fd);
+        })
+
+
+
     });
 
-
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            //注意：如果向服务器提交FormData格式数据
+            //必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function(res) {
+                if (res.status !== 0) {
+                    console.log("失败");
+                    return layer.msg('发布文章失败！');
+                }
+                layer.msg('发布文章成功！');
+                //发布文章成功后，跳转到文章列表
+                location.href = 'art_list.html';
+            }
+        });
+    }
 })
